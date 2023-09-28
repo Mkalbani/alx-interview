98e1a0cb67b5ec9ff25da8570404f1e7879c8a40
Mkothm <mkothman.jr@gmail.com>
2023-09-28 21:56:58 +0000

    new

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/python3  # Shebang line indicating the Python interpreter to use
+
 def pascal_triangle(n):
     if n <= 0:
-        return []
+        return []  # Return an empty list if n is less than or equal to 0
 
-    triangle = [[1]]  # Initialize with the first row [1]
+    triangle = [[1]]  # Initialize a list with the first row [1]
 
-    for _ in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]  # First element is always 1
+    for _ in range(1, n):  # Loop from the second row (row index 1) to the nth row
+        prev_row = triangle[-1]  # Get the last row of the current triangle
+        new_row = [1]  # Create a new row with the first element as 1
 
-        for i in range(1, len(prev_row)):
+        for i in range(1, len(prev_row)):  # Loop through the previous row
+            # Calculate the middle elements by adding two adjacent elements from the previous row
             new_row.append(prev_row[i - 1] + prev_row[i])
 
-        new_row.append(1)  # Last element is always 1
+        new_row.append(1)  # Add the last element of the new row, which is always 1
         triangle.append(new_row)  # Append the new row to the triangle
 
-    return triangle
+    return triangle  # Return the generated Pascal's triangle
--- 0x00-pascal_triangle/README.md
+++ 0x00-pascal_triangle/README.md
@@ -1,1 +1,22 @@
-pascal
+# Pascal's Triangle Generator
+
+This Python script generates Pascal's triangle of a specified height `n` and returns it as a list of lists of integers. Pascal's triangle is a triangular array of binomial coefficients, where each number is the sum of the two numbers directly above it.
+
+## Usage
+
+To use the Pascal's Triangle Generator, follow these steps:
+
+1. Ensure you have Python installed on your system.
+
+2. Clone this repository or download the `pascal_triangle.py` file.
+
+3. Import the `pascal_triangle` function into your Python script or interactive environment.
+
+4. Call the `pascal_triangle` function with the desired height `n` as an argument to generate Pascal's triangle.
+
+```python
+from pascal_triangle import pascal_triangle
+
+# Generate Pascal's triangle of height 5
+result = pascal_triangle(5)
+print(result)
