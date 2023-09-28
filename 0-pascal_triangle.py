059988db67ff2825bf7ed54c4db44f6db6d426f6
Mkothm <mkothman.jr@gmail.com>
2023-09-28 21:45:57 +0000

    pascal

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize with the first row [1]
+
+    for _ in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]  # First element is always 1
+
+        for i in range(1, len(prev_row)):
+            new_row.append(prev_row[i - 1] + prev_row[i])
+
+        new_row.append(1)  # Last element is always 1
+        triangle.append(new_row)  # Append the new row to the triangle
+
+    return triangle
