92922186d7197a4535ff4df19e1e4dea0e1ed7fe
mkalbani <mkothman.jr@gmail.com>
2024-12-13 14:49:01 -0800

    corrected

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,54 +1,50 @@
 #!/usr/bin/python3
 """
-Defining the prime game
+Prime Game: Determine the winner of a game where players pick primes optimally.
 """
 
 
 def isWinner(x, nums):
-    def sieve_of_eratosthenes(n):
-        """
-        constricting the varibales
-        """
-        primes = [True] * (n + 1)
-        primes[0], primes[1] = False, False
+    """
+    Determines the winner of the game after x rounds.
 
-        p = 2
-        while p * p <= n:
-            if primes[p]:
-                for i in range(p * p, n + 1, p):
-                    primes[i] = False
-            p += 1
+    Args:
+        x (int): Number of rounds.
+        nums (list): List of integers representing the range of numbers.
 
-        return [i for i in range(n + 1) if primes[i]]
+    Returns:
+        str or None: The name of the player
+        with the most wins ("Maria" or "Ben"),
+                     or None if there's no winner.
+    """
+    if not nums or x <= 0:
+        return None
 
-    def calculate_winner(n):
-        """
-        defines the winner function
-        """
-        primes = sieve_of_eratosthenes(n)
-        if n in primes:
-            return "Maria"  # Maria wins if the given number itself is prime
+    max_num = max(nums)
+    sieve = [True] * (max_num + 1)
+    sieve[0] = sieve[1] = False
 
-        for prime in primes:
-            if n % prime == 0:
-                return "Ben"  # Ben wins if any prime factor is present
+    for i in range(2, int(max_num ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_num + 1, i):
+                sieve[j] = False
 
-        return "Maria"  # Maria wins if no prime factor found
-
+    primes_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        primes_count[i] = primes_count[i - 1] + (1 if sieve[i] else 0)
 
     maria_wins = 0
     ben_wins = 0
 
-    for i in range(x):
-        winner = calculate_winner(nums[i])
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
+    for n in nums:
+        if primes_count[n] % 2 == 0:
             ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
-        return "Ben"
-    elif ben_wins > maria_wins:
         return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
         return None
